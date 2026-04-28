168c70426f36434140072c522c3f1cadc2919e40
nickslept <nsleptsov@hotmail.com>
2026-04-28 18:36:24 -0400

    small changes in naming conventions

--- fetch.py
+++ fetch.py
@@ -2,13 +2,13 @@
 #api key hidden for obvious reasons
 import requests
 
-QUERY_ID = "your_query_id"
-API_KEY = "your_api_key"
+QUERY_ID = ""
+API_KEY = ""
 
 response = requests.get(
     f"https://api.dune.com/api/v1/query/{QUERY_ID}/results/csv",
     headers={"X-Dune-Api-Key": API_KEY}
 )
 
-with open("dune_query_results.csv", "w") as file:
+with open("dune_query.csv", "w") as file:
     file.write(response.text)
